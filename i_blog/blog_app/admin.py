c0996f6686323ab3f8a2faae46dbf02707e1c546
khot-abhishek <abhishekrkhot@gmail.com>
2022-10-12 17:18:56 +0530

    Created models "Category & Post" for handling our blog data and registerd them for admin panel

--- i_blog/blog_app/admin.py
+++ i_blog/blog_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdminModel(admin.ModelAdmin):
+    list_display = ['title', 'description', 'created']
+    search_fields = ['title']
+    
+    
+@admin.register(Post)
+class PostAdminModel(admin.ModelAdmin):
+    list_display = ['title', 'created_on']
+    search_fields = ['title']
--- i_blog/blog_app/models.py
+++ i_blog/blog_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    url = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category/')
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='post/')
+    url = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
